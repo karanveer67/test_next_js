d8387df906de86d5e944bf26b245ac378c902537
karanveer67 <karangill16629@gmail.com>
2024-10-25 13:11:26 +0530

    new file

---
+++ app/page.tsx
@@ -0,0 +1,303 @@
+
+export default function Home() {
+  return (
+    <div className="main_body">
+      <div className="preveiw-header">
+        <div className="envato_logo">
+          <a className="header_envato" href="https://themeforest.net/item/etrade-multipurpose-ecommerce-nextjs-template/48473978">Envato Market</a>
+        </div>
+        <div className="preveiw_action">
+          <div className="preveiw_action_button">
+            <a className="headeer_button" href="https://themeforest.net/checkout/105339269/create_account">Buy now</a>
+          </div>
+        </div>
+      </div>
+
+      <header className="header_style">
+        <div className="header_offer">
+          <div className="offer_content">
+            <p className="content_para">Introductory Offer Get Upto 50% Off</p>
+            <a className="content_anchor" target="_blank" href="https://themeforest.net/item/etrade-multipurpose-ecommerce-nextjs-template/48473978"> Buy Now</a>
+          </div>
+        </div>
+      </header>
+      <div className="mainmenu">
+        <div className="container">
+          <div className="header_navbar">
+            <div className="header_brand">
+              <a className="logo" href="/"><img loading="lazy" alt="site" width="150" height="40" style={{ color: "transparent" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Flogo%2Flogo.png&w=256&q=75" />
+              </a>
+            </div>
+            <div className="header_main_menu">
+              <nav className="mainmenu_nav">
+                <ul className="mainmenu">
+                  <li><a href="#demos" className="smoth-animation">Demo</a></li>
+                  <li><a href="#features" className="smoth-animation">Features</a></li>
+                  <li><a href="https://new.axilthemes.com/docs/etrade-react/" className="smoth-animation">Documentation</a></li>
+                  <li><a href="https://support.axilthemes.com/support/login" className="smoth-animation">Support</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div className="header_action">
+              <div className="header_btn">
+                <a className="menu_btn" href="">Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="banner_wrapper">
+        <div className="banner_area">
+          <div style={{ padding: "0 15px" }}>
+            <span style={{ color: "#ff497c", fontSize: "14px", fontWeight: "bold" }}>eCommerce</span>
+            <h1 style={{ fontSize: "44px", fontWeight: "bold", width: "65%", marginBottom: "20px" }}>eTrade Multipurpose eCommerce Next JS Template</h1>
+            <div className="theme_breif">
+              <div className="subtitle_box">
+                <span className="subtitle">Demo Website</span>
+                <div>
+                  <h2 style={{ color: "#3577f0", fontSize: "48px", lineHeight: "1", fontWeight: "700" }}>5+</h2>
+                </div>
+              </div>
+              <div className="subtitle_box">
+                <span className="subtitle">Inner Page</span>
+                <div>
+                  <h2 style={{ color: "#3577f0", fontSize: "48px", lineHeight: "1", fontWeight: "700" }}>25+</h2>
+                </div>
+              </div>
+              <div className="subtitle_box">
+                <span className="subtitle">Elements</span>
+                <div>
+                  <h2 style={{ color: "#3577f0", fontSize: "48px", lineHeight: "1", fontWeight: "700" }}>30+</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style={{ padding: "0 15px" }}>
+            <div style={{ marginBottom: "-82px" }}>
+              <img className="computerimg" alt="mockup" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fbanner-mockup.png&w=1080&q=75" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="label_box">
+        <label htmlFor="">Powered By</label>
+        <ul className="icon_list">
+          <li><img className="icon_img" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fnext.png&w=32&q=75" alt="nextjs" /> </li>
+          <li><img className="icon_img" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Freact.png&w=32&q=75" alt="reactjs" /></li>
+          <li><img className="icon_img" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fnpm.png&w=32&q=75" alt="npm" /></li>
+          <li><img className="icon_img" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fscss.png&w=32&q=75" alt="sass" /></li>
+          <li><img className="icon_img" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fbootstrap.png&w=32&q=75" alt="bs" /></li>
+        </ul>
+      </div>
+      <div className="cards">
+        <div className="card_container">
+          <div className="demo_area"></div>
+          <div className="home_header">
+            <span style={{ color: "#ff497c", fontSize: "14px", fontWeight: "bold" }}>Home</span>
+            <h1 style={{ fontSize: "44px", fontWeight: "bold", width: "35%", marginBottom: "20px" }}>Our Template is Just
+              Ready to Use</h1>
+          </div>
+          <div className="cards_img">
+            <div className="hover_effect" >
+              <div className="thumb"><img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fhome-01.png&w=640&q=75" alt="img" /></div>
+              <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Electronics</a></h5>
+            </div>
+            <div className="hover_effect" >
+              <div className="thumb"><img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fhome-04.png&w=640&q=75" alt="img" /></div>
+              <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Jewellery</a></h5>
+            </div>
+            <div className="hover_effect" >
+              <div className="thumb"><img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fhome-03.png&w=640&q=75" alt="img" /></div>
+              <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Fashion</a></h5>
+            </div>
+            <div className="hover_effect" >
+              <div className="thumb"><img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fhome-05.png&w=640&q=75" alt="img" /></div>
+              <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Furniture</a></h5>
+            </div>
+            <div className="hover_effect" >
+              <div className="thumb"><img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fhome-02.png&w=640&q=75" alt="img" />
+
+                <div className="button-container">
+                  <a href="https://etrade-delta.vercel.app/home/electronics" className="view-demo-button">View Demo</a>
+                </div>
+
+              </div>
+              <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">NFT</a></h5>
+            </div>
+            <div>
+              <div className="thumb"><img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fcomming-soon.png&w=640&q=75" alt="img" /></div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+      <div className="inner_page">
+        <div className="inner_header">
+          <span style={{ color: "#8c71db", fontSize: "14px", fontWeight: "bold" }}>Inner page</span>
+          <h1 style={{ fontSize: "39px", fontWeight: "bold", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>Eye Catching Pre-build<br></br>
+            Inner Pages</h1>
+        </div>
+        <div className="cards_img">
+          <div className="hover_effect" >
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fshop.png&w=640&q=75" alt="img" /></div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Shop</a></h5>
+          </div>
+          <div className="hover_effect" >
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fshop.png&w=640&q=75" alt="img" /></div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Shop with Sidebar</a></h5>
+          </div>
+          <div className="hover_effect" >
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fvariation-1.png&w=640&q=75" alt="img" /></div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Product Variation 1</a></h5>
+          </div>
+          <div className="hover_effect" >
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fvariation-2.png&w=640&q=75" alt="img" /></div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Product Variation 2</a></h5>
+          </div>
+          <div className="hover_effect" >
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fvariation-3.png&w=640&q=75" alt="img" />
+
+              <div className="button-container">
+                <a href="https://etrade-delta.vercel.app/home/electronics" className="view-demo-button">View Demo</a>
+              </div>
+
+            </div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Product Variation 3</a></h5>
+          </div>
+          <div>
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fvariation-4.png&w=640&q=75" alt="img" /></div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Product Variation 4</a></h5>
+          </div>
+          <div>
+            <div className="thumb"><img style={{ height: "410px", width: "410px" }} src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fvariation-5.png&w=640&q=75" alt="img" /></div>
+            <h5 className="title"><a href="https://etrade-delta.vercel.app/home/electronics">Product Variation 5</a></h5>
+          </div>
+
+
+        </div>
+      </div>
+
+      <div className="features_container">
+        <div className="inner_header">
+          <span style={{ color: "#ff497c", fontSize: "16px", fontWeight: "bold" }}>Features</span>
+          <h1 style={{ fontSize: "39px", fontWeight: "bold", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>We have Impressive
+            <br></br>Features
+          </h1>
+        </div>
+        <div className="feat_cards">
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-10.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Powered by Next.js</h3>
+            <p className="boxpara">Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-11.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Reusable Components</h3>
+            <p className="boxpara">All the Components of this template are reusable. We made eTrade using functional components. You can reuse any components</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-05.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Fast Loading Speed</h3>
+            <p className="boxpara">The website speed is very important. Hence we’ve created eTrade in a way that your website will load fast, and you will get better performance.</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-01.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Fully Responsive <br />
+              Layout</h3>
+            <p className="boxpara">All the pages of this theme are responsive. We used Bootstrap framework to build the website. It’s the best in class.</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-02.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>25+ Resourceful<br />
+              Pages</h3>
+            <p className="boxpara">All the pages are created based on real content that you will need to run your business. Change image and text and you’re good to go!</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-09.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Fully<br />
+              Customizable</h3>
+            <p className="boxpara">Our template is fully customizable. You can change color combination, fonts, icons, contents, images etc. You can also add custom css.</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-03.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Font Awesome 5<br />
+              Pro Icons</h3>
+            <p className="boxpara">This template comes with licensed premium quality icons from FontAwesome. All the icons are font based and ready to match the quality of any HQ screen.</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-04.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Google<br />
+              Font</h3>
+            <p className="boxpara">A vast collection of Google fonts are integrated with the theme. You can use any of them that goes with your branding.</p>
+          </div>
+
+          <div className="service_box">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "100%", height: "auto", marginBottom: "15px" }}>
+              <img src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Ffeature-icon-05.png&w=48&q=75" alt="nxtjs" />
+            </div>
+            <h3 style={{ fontSize: "24px", marginBottom: "15px", fontWeight: "700" }}>Lifetime<br />
+              Update</h3>
+            <p className="boxpara">Purchase our theme only once and get lifetime updates. We keep updating our products to stay up to date with latest trends and technology.</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="container_box">
+        <div className="box_container">
+          <div className="boxone">
+            <span style={{ fontSize: "28px", fontWeight: "700", display: "block", marginBottom: "10px" }}>Online Documentation</span>
+            <span style={{ display: "block", marginBottom: "35px" }}>Well organized and upto date</span>
+            <span style={{ fontSize: "16px", fontWeight: "700", display: "block" }}>Veiw Documentation </span>
+          </div>
+          <div className="boxtwo">
+            <span style={{ fontSize: "28px", fontWeight: "700", display: "block", marginBottom: "10px" }}>Dedicated Support</span>
+            <span style={{ display: "block", marginBottom: "35px" }}>We responsenin within 1 bussiness day. <br /> Weekends excluded</span>
+            <span style={{ fontSize: "16px", fontWeight: "700", display: "block" }}>Create Support </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="ready_container">
+        <div className="ready_header">
+          <span style={{ color: "#8c71db", fontSize: "14px", fontWeight: "bold" }}>Are you Ready</span>
+          <h1 style={{ fontSize: "39px", fontWeight: "bold", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>Let’s Start Your Business Today!</h1>
+          <div className="ready_btn"> <a style={{ backgroundColor: "#3577f0", padding: "15px 40px", borderRadius: "6px" }} href="https://themeforest.net/item/etrade-multipurpose-ecommerce-nextjs-template/48473978">Buy Now</a></div>
+          <div className="phoneimg">
+            <img width="1230" height="540" src="https://etrade-delta.vercel.app/_next/image?url=%2Fimages%2Fpreview%2Fcta-mockup.png&w=1920&q=75" alt="" />
+          </div>
+        </div>
+      </div>
+      <footer className="footer">© 2023. All rights reserved by Axilthemes.</footer>
+
+    </div>
+  );
+}
